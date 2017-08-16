63827fed8bfdf0266c044acf2c938fe1c1291342
Maria Korobeynikova <siverka@gmail.com>
2017-08-16 16:59:30 +0300

    updated statuses matching

--- sources/get_jira_changes.py
+++ sources/get_jira_changes.py
@@ -1,15 +1,17 @@
 from jira import JIRA
-
 from sources.progress import progress
+
+statuses = {'pass': ['Resolved', 'Closed'],
+            'skip': ['Resolved', 'Closed'],
+            'fail': ['Open', 'In Progress', 'Reopened'],
+            'blocked': ['Open', 'In Progress', 'Reopened'],
+            '': ['Open', 'In Progress', 'Reopened'],
+            'query': ['Open', 'In Progress', 'Reopened']}
 
 
 def get_jira_changes(tests: list, server: str, auth: tuple):
     jira = JIRA(server, basic_auth=auth)
-    statuses = {'blocked': ['Open', 'In Progress', 'Reopened'],
-                'fail': ['Open', 'In Progress', 'Reopened'],
-                '': ['Open', 'In Progress', 'Reopened'],
-                'pass': ['Resolved', 'Closed'],
-                'skip': ['Open', 'In Progress', 'Reopened']}
+    global statuses
     changes = []
     i, n = 0, len(tests)
     for check in tests:
